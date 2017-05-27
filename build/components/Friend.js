4edd657c3abea5fdf9c2a814204b4f72dd6a67fc
L.A. Westley <lutuawestley@gmail.com>
2017-05-27 12:06:35 -0600

    friendList

---
+++ build/components/Friend.js
@@ -0,0 +1,19 @@
+
+class Friend extends React.Component {
+	
+render() {
+	return (
+		<li className='friend'>
+			<img className="profile-pic" src={this.props.pictureUrl} />
+
+				<h3>{this.props.name}</h3>
+
+				<div className="location">
+					Location: {this.props.currentLocation.city}, {this.props.currentLocation.state}, {this.props.currentLocation.country}
+				</div>
+		</li>
+	);
+}
+}
+
+export default Friend;
