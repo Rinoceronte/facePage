3427f3c31395b4d9830f24aff678c0ac523c0427
L.A. Westley <lutuawestley@gmail.com>
2017-05-29 18:33:47 -0600

    Friendlist.js updates

--- build/components/FriendsList.js
+++ build/components/FriendsList.js
@@ -1,30 +1,77 @@
 import React from "react";
-import Friend from "./Friend";
+import axios from 'axios';
 
 export default class FriendsList extends React.Component {
     constructor() {
         super();
 
+        this.handleUserHome = this.handleLogout.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+
         this.state = {
-            searchText: "",
-            orderBy: "name",
-            order: "ascending"
+            users: []
         };
     }
 
-    handleChange(field, event) {
-        this.setState({[field]: event.target.value});
+    componentWillMount() {
+        axios.get('/users').then(data => {
+            //data will have an object of response.data.users list...
+            console.log(response.data);
+            if (response.data && response.data.users) {
+                this.setState({
+                    users: response.data.users
+                });
+            }
+        });
+
     }
 
-    render() {
-        return(
+    handleChange(field, event) {
+        this.setState({ [field]: event.target.value });
+    }
 
+    handleUserHome(){
+        this.props.history.push('/secure');
+    };
+
+    handleLogout(){
+        this.props.history.push('/');
+        this.props.logout();
+    };
+
+    render() {
+
+        let friendList = [];
+        if (this.state.users && this.state.users.length > 0) {
+            userList = this.state.users.map(user => {
+                if (user.friendList && user.friendList.length > 0) {
+                    friendList = user.friendList.map(task => <div key={task._id + '-task'}>{task.description}</div>);
+                }
+                return (
+                    <div key={user._id + '-user'}>
+                        {user.name}
+                        <h3>Friend List</h3>
+                        {friendList}
+                    </div>
+                )
+            });
+        };
+
+
+        return (
             <div>
-                <Link to="/secure">Home</Link><br/>
-                <a href="#">Log Out</a>
+                <button type="button" onClick={this.handleLogout}>Home</button><br/>
+                <button type="button" onClick={this.handleLogout}>Log Out</button>
+                {/*<a href="#" onClick={this.handleLogout}>Log Outtt</a>*/}
+                <Route exact path="/secure" render={() => <PrivateHomepage {...this.props} />}/>
+            </div>)
+
+            (<div>
+                <h1>Friend List</h1>
+                {/*loop*/}
             </div>
             );
-            
+
         // const friendsList = friends.map(friend => (
         //     <Friend
         //         currentLocation={friend.current_location || {}}
