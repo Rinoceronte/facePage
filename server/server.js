9cb881a868be9429289233dfc9519059896b7d92
L.A. Westley <lutuawestley@gmail.com>
2017-06-03 00:55:22 -0600

    Revert "Heroku deployment"
    This reverts commit 3d74eab306a9e3da04f80a037c0b7856b133f424.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 'use strict';
-//import env from './env';
+import env from './env';
 import express from 'express';
 import handlebars from 'express-handlebars';
 import http from 'http';
@@ -28,7 +28,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 // connection string. 
-mongoose.connect(`mongodb://${process.env.DB}`);
+mongoose.connect(`mongodb://localhost/${process.env.DB}`);
 
 Routes(app);
 
